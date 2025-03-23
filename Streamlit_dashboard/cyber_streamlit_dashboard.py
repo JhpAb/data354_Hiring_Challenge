b0967ae65e5fbaa7f4aebc03a944e62cf5fbc232
ABBE Jean Pierre <128245163+JhpAb@users.noreply.github.com>
2025-03-23 14:51:30 +0000

    Update cyber_streamlit_dashboard.py

--- Streamlit_dashboard/cyber_streamlit_dashboard.py
+++ Streamlit_dashboard/cyber_streamlit_dashboard.py
@@ -139,8 +139,7 @@ elif page == "🏆 Analyse des auteurs":
     st.pyplot(fig)
     
     # 📊 Likes et Shares des Top 10 Auteurs (2 colonnes côte à côte)
-    
-    st.write("N.B : Les donées de 'Likes' & de 'Shares' on été générées de manière arbitraire vu que nous ne pouvions les colleceter.")
+    st.markdown("**N.B :** Les données de 'Likes' et de 'Shares' ont été générées de manière arbitraire, car nous ne pouvions pas les collecter.")
     
     st.subheader("Likes et Shares des Top 10 Auteurs")
     col1, col2 = st.columns(2)
